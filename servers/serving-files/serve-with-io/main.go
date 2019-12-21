edbdc872263ac8f7dfdad1fdbe61b49e33b9643a
tevin-morake <tevin@rapidtrade.biz>
2019-12-21 17:12:42 +0200

    serving image file with io.Copy

---
+++ servers/serving-files/serve-with-io/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		f, err := os.Open("img/league.png")
+		if err != nil {
+			fmt.Fprintf(w, "<h1>Dude, theres a problem loading your picture</h1>")
+		}
+
+		io.Copy(w, f)
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
