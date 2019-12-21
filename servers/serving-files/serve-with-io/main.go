9c01ad75d1bf5de5aedacb135ed76cab367377bd
tevin-morake <tevin@rapidtrade.biz>
2019-12-21 17:31:41 +0200

    added file close

--- servers/serving-files/serve-with-io/main.go
+++ servers/serving-files/serve-with-io/main.go
@@ -11,6 +11,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		f, err := os.Open("img/league.png")
+		defer f.Close()
 		if err != nil {
 			fmt.Fprintf(w, "<h1>Dude, theres a problem loading your picture</h1>")
 		}
