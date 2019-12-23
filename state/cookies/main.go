c0be7213f2f5ea241d22fa8ac9cf60dca43ad86d
tevin-morake <tevin@rapidtrade.biz>
2019-12-23 16:23:20 +0200

    added method to count how many times user has visited site

--- state/cookies/main.go
+++ state/cookies/main.go
@@ -4,16 +4,34 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
+	// Changed this to a counter that checks how many times a person visits the /set uri
+	// to delete a cookie, just set MaxAge = -1. Done!
 	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
-		http.SetCookie(w, &http.Cookie{
-			Name:  "ScriptWhizCookie",
-			Value: "Doranbolt",
-		})
+		c, err := r.Cookie("ScriptWhizCookieSiteVisit")
+		if err != nil {
+			http.SetCookie(w, &http.Cookie{
+				Name:  "ScriptWhizCookieSiteVisit",
+				Value: "1",
+			})
+		} else {
+			stringCookie := c.Value
+			intCookieVal, err := strconv.Atoi(stringCookie)
+			if err != nil {
+				fmt.Println("Failed to convert string cookie to number")
+			}
+			intCookieVal++
+			strCookie := strconv.Itoa(intCookieVal)
+			http.SetCookie(w, &http.Cookie{
+				Name:  "ScriptWhizCookieSiteVisit",
+				Value: strCookie,
+			})
+		}
 	})
 
 	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
